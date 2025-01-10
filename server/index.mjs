5379a1b076e6d4ba9e2ff197dd444b0bcb5e1972
parthkathiriya90 <parth@sourcenettechnology.in>
2025-01-10 14:39:58 +0530

    Set up

--- server/index.mjs
+++ server/index.mjs
@@ -34,9 +34,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Serve the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // Start the server
