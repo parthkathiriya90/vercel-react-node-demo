08aa262e2cb34c581c08a338537f374611a07bec
parthkathiriya90 <129510800+parthkathiriya90@users.noreply.github.com>
2025-01-13 14:27:26 +0530

    Resolve vercel issue.

--- index.mjs
+++ index.mjs
@@ -18,7 +18,7 @@ connectToDatabase();
 
 const app = express();
 const PORT = process.env.PORT;
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 
@@ -44,28 +44,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`.blue);
 });
-
-// const express = require('express');
-// const path = require('path');
-// const axios = require('axios');
-
-// const app = express();
-
-// // Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// // Handle requests by serving index.html for all routes
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-// });
-
-// // API endpoint
-// app.use('/api/products', async (req, res) => {
-//   const response = await axios.get('https://fakestoreapi.com/products');
-//   res.send(response.data);
-// });
-
-// // Start the server
-// app.listen(process.env.PORT, () => {
-//   console.log('Server is running on port ' + process.env.PORT);
-// });
