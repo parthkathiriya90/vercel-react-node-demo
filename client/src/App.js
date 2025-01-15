909386b5bc3d9dd3d69e9048f45a1271de6c2bf5
parthkathiriya90 <129510800+parthkathiriya90@users.noreply.github.com>
2025-01-15 18:09:47 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const [data, setData] = useState([]);
