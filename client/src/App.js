9d21d80e4093a0ac4735e9e50e05db9372911398
parthkathiriya90 <129510800+parthkathiriya90@users.noreply.github.com>
2025-01-15 18:04:39 +0530

    Update some codes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/user');
+        console.log(response, "responseresponseresponseresponseresponseresponseresponseresponseresponseresponseresponse");
+
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +41,15 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+        <h1>Data from API</h1>
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
