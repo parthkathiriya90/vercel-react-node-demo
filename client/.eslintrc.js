070094053155c050576bbd446ecf905f24840b61
parthkathiriya90 <129510800+parthkathiriya90@users.noreply.github.com>
2025-01-15 18:39:02 +0530

    Resolve issues.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,10 +1,14 @@
+require('dotenv').config(); // Load environment variables from the .env file
+
 module.exports = {
-    "env": {
-        "production": {
-            "rules": {
-                "react-hooks/exhaustive-deps": "off",
-                "no-unused-vars": "warn"
-            }
-        }
+  "overrides": [
+    {
+      "files": ["**/*.js", "**/*.jsx"],
+      "rules": {
+        // Adjust rules based on the NODE_ENV value from the .env file
+        "react-hooks/exhaustive-deps": process.env.NODE_ENV === 'production' ? 'off' : 'warn',  // Disable in production
+        "no-unused-vars": process.env.NODE_ENV === 'production' ? 'warn' : 'error',  // Warn in production
+      }
     }
+  ]
 };
