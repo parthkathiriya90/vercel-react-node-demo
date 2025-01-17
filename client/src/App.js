aeb04338422e015eef78b09843b04c10f9054909
Kavita Bavadiya <kavita@sourcenettechnology.in>
2025-01-17 16:02:47 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 // import Home from './page/home';
-import Home from './page/Home.js';
+import Home from "../src/page/Home";
 import '../src/style/style.css';
 import '../src/style/style2.css';
 import Service from './page/service';
