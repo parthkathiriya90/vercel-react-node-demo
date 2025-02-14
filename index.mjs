86c3f660053891b773bac92ad431fecdae2ae584
parthkathiriya90 <129510800+parthkathiriya90@users.noreply.github.com>
2025-02-14 14:52:16 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -30,7 +30,8 @@ app.use(express.json());
 
 // Swagger documentation setup
 const options = {
-  customCss: '.swagger-ui .topbar { display: none }',
+  customCss:
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
   customSiteTitle: 'My API Documentation',
 };
 
